c5e8efa3902082b7935a9841a2581bc340090777
wilsonz0 <105890511+wilsonz0@users.noreply.github.com>
2023-07-18 21:42:55 -0400

    initial commit with backend and autogenerated frontend react app

---
+++ backend/server.js
@@ -0,0 +1,12 @@
+require('dotenv').config()
+
+const express = require("express")
+const app = express()
+
+app.get('/', (req, res) => {
+    res.json({mssg: 'Hello World!'})
+})
+
+app.listen(process.env.PORT, () => {
+    console.log("Listening on port", process.env.PORT)
+})
