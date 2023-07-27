fda797f2d40bfaa475bfdbf2515d27bff9fe8ea1
wilsonz0 <105890511+wilsonz0@users.noreply.github.com>
2023-07-26 22:19:19 -0400

    set up an initial schedules routes and basic operations

---
+++ backend/routes/schedules.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const router = express.Router()
+
+// GETs
+router.get('/', (req, res) => {
+    res.json({mssg: 'GET all schedules'})
+})
+
+router.get('/:id', (req, res) => {
+    res.json({mssg: "GET a single schedule"})
+})
+
+// POST
+router.post('/', (req, res) => {
+    res.json({mssg: 'POST a new schedule'})
+})
+
+// DELETE 
+router.delete('/:id', (req, res) => {
+    res.json({mssg: "DELETE a single schedule"})
+})
+
+// PATCH (aka UPDATE)
+router.patch('/:id', (req, res) => {
+    res.json({mssg: "PATCH a single schedule"})
+})
+
+module.exports = router
