9fac9a431d4c79e14a4962586d827dbdd958e7b3
wilsonz0 <105890511+wilsonz0@users.noreply.github.com>
2023-07-26 22:19:19 -0400

    set up an initial schedules.js middleware

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,17 @@ require('dotenv').config()
 const express = require("express")
 const app = express()
 
+const schedules = require('./routes/schedules')
+
+// middleware
+app.use((req, res, next) => {
+    console.log(req.path, req.method)
+    next()
+})
+
+app.use('/schedules', schedules)
+
+// for the root address
 app.get('/', (req, res) => {
     res.json({mssg: 'Hello World!'})
 })
