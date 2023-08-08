504b5132247166971d12c5adda2eb3d1471a2260
wilsonz0 <105890511+wilsonz0@users.noreply.github.com>
2023-08-07 21:35:35 -0400

    added buttons for two functions: shared schedule and shared timer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,29 @@
 import './App.css';
 
+function scheduleClicked() {
+  console.log("schedule clicked")
+}
+
+function timerClicked() {
+  console.log("timer clicked")
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>ScheduleSync</h1>
       </header>
-      <p>
-          This react app will allow users to share personal schedules 
-          with each other. Also users can also create a special schedule 
-          that can be edited by anyone that the creator chooses to shared to. 
-          <br></br><br></br>
-          There will be also other potential features that allow users to 
-          share more time or schedule-related with each other to connect 
-          friends and families together and help them find free-time 
-          together.
-        </p>
+      
+      <div className="buttons">
+        <button id="schedule" onClick={scheduleClicked}>
+          <img src={require('./images/calendar-icon.png')} alt="Button"/>
+        </button>
+        <button id="timer" onClick={timerClicked}>
+          <img src={require('./images/timer-icon.png')} alt="Button"/>
+        </button>
+      </div>
+
     </div>
   );
 }
