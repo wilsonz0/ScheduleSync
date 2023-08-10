d4b65998433c200a990f3c4178d2ad3e20eedb6b
wilsonz0 <105890511+wilsonz0@users.noreply.github.com>
2023-08-09 22:10:58 -0400

    add proper basic format to timer page

--- frontend/src/pages/Timer.js
+++ frontend/src/pages/Timer.js
@@ -1,10 +1,44 @@
-// import { useEffect, userState } from 'react'
+import { useEffect, useState } from 'react'
+import '../styles/Timer.css'
+
+// testing
+import testData from "../test.json"
 
 const Timer = () => {
+    const [timers, setTimers] = useState(null)
+
+    useEffect(() => {
+        const fetchTimers = async () => {
+            // this is a test to lessen the amount of requests to the database
+            setTimers(testData)
+
+            // uncomment below for proper fetching
+            // const response = await fetch('/timers')
+            // const json = await response.json()
+
+            // if (response.ok) {
+            //     setTimers(json)
+            // }
+        }
+
+        fetchTimers()
+    }, [])
+
     return (
         <div className="timer">
             <h2>Timers</h2>
             <p> This will display all the timers and allow user to share their timer with others.</p>
+            <div className='timers'>
+                {timers && timers.map((timer) => (
+                    <p key={timer._id}>
+                        <h3>{timer.name}</h3> 
+                        <span>own by ownerID: {timer.ownerID}</span>
+                        <p>
+                            starting time was {timer.startingTime} sec and there is {timer.timerLength} sec left.
+                        </p>
+                    </p>
+                ))}
+            </div>
         </div>
     )
 }
