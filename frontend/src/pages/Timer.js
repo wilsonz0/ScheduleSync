69230bc58b52747210badf7a4342e5768f5e55ce
wilsonz0 <105890511+wilsonz0@users.noreply.github.com>
2023-10-07 23:52:35 +0900

    add the functionality of creating a new timer

--- frontend/src/pages/Timer.js
+++ frontend/src/pages/Timer.js
@@ -8,6 +8,38 @@ import testData from "../test.json"
 const Timer = () => {
     const [timers, setTimers] = useState(null)
 
+    const [newHours, setNewHours] = useState('');
+    const [newMinutes, setNewMinutes] = useState('');
+    const [newSeconds, setNewSeconds] = useState('');
+
+    function addNewTimer() {
+        if (!newHours || !newMinutes || !newSeconds) {
+            alert('Please enter valid time values for the new timer.');
+            return;
+        }
+    
+        const totalSeconds = parseInt(newHours) * 3600 +
+                            parseInt(newMinutes) * 60 +
+                            parseInt(newSeconds);
+        
+        const newTimer = {
+            name: 'the fourth',
+            ownerID: 4,
+            startingEpoch: 50,
+            timerLength: totalSeconds,
+            
+        };
+
+        // TODO: add to database (which should update the screen immediately)
+        testData.push(newTimer)         // this is a test
+
+        setNewHours('');
+        setNewMinutes('');
+        setNewSeconds('');
+    }
+
+
+
     useEffect(() => {
         const fetchTimers = async () => {
             // this is a test to lessen the amount of requests to the database
@@ -35,8 +67,14 @@ const Timer = () => {
             </header>
             
             <div id="createBox">
-                <input type="text" id="countdownInput"/>
-                <button id="createButton">Create</button>
+                <div className="inputWrapper">
+                    <input id="hours" placeholder="h" value={newHours} onChange={(e) => setNewHours(e.target.value)}/>
+                    <span>:</span>
+                    <input id="minutes" placeholder="m" value={newMinutes} onChange={(e) => setNewMinutes(e.target.value)}/>
+                    <span>:</span>
+                    <input id="seconds" placeholder="s" value={newSeconds} onChange={(e) => setNewSeconds(e.target.value)}/>
+                </div>
+                <button id="createButton" onClick={addNewTimer}>Create</button>
             </div>
             <div className='timers'>
                 {timers && timers.map((timer) => (
